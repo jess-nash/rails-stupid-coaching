437d4d518d6a18522f559eed508d856b948f9b12
Jessica Nash <jeenash2@gmail.com>
2022-06-28 22:12:36 +0900

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question].downcase == 'i am going to work' || params[:question].downcase == "i'm going to work"
+      @answer = 'Great!'
+    elsif params[:question].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ROUTE
+  # Write a simple route to serve the GET /ask HTTP request to the ask action of the questions controller.
+  # Generic syntax: verb 'path', to: 'controller#action'
+
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
